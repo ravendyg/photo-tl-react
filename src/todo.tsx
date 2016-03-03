5085702e6a92604a056dd517617a52b67118d054
ravendyg <jaroslav.dashkevich@gmail.com>
2016-03-03 12:31:42 +0600

    Add router

--- src/todo.tsx
+++ src/todo.tsx
@@ -4,16 +4,82 @@
 const React: IReact = vendor.React;
 const ReactDom: IReactDom = vendor.ReactDom;
 
+var Router = vendor.ReactRouter.Router
+var Route = vendor.ReactRouter.Route
+var Link = vendor.ReactRouter.Link
+var hashHistory = vendor.ReactRouter.hashHistory;
+
 const store: IStore = require('./store.ts');
 
 // view components
 import {TodoApp} from './components/TodoApp.tsx';
 
-// initialization
+// // initialization
+// ReactDom.render(
+//     <TodoApp />,
+//     document.getElementById(`root`)
+// );
+
+class App extends React.Component {
+    constructor(){ super();}
+    
+    render() {
+        return (
+        <div>
+            <h1>App</h1>
+            {/* change the <a>s to <Link>s */}
+            <ul>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/inbox">Inbox</Link></li>
+            </ul>
+
+            {/*
+            next we replace `<Child>` with `this.props.children`
+            the router will figure out the children for us
+            */}
+            {this.props.children}
+        </div>
+        )
+    }
+}
+
+class About extends React.Component {
+    constructor(){ super();}
+    
+    render() {
+        return (
+        <div>
+            About
+            <Link to="/">Home</Link>
+        </div>
+        )
+    }
+}
+
+class Inbox extends React.Component {
+    constructor(){ super();}
+    
+    render() {
+        return (
+        <div>
+            Inbox
+            <Link to="/">Home</Link>
+        </div>
+        )
+    }
+}
+
+const routes = {
+  path: '/',
+  component: App,
+  indexRoute: { component: TodoApp },
+  childRoutes: [
+    { path: 'about', component: About },
+    { path: 'inbox', component: Inbox },
+  ]
+}
+
 ReactDom.render(
-    <TodoApp />,
+    <Router history={hashHistory} routes={routes} />,
     document.getElementById(`root`)
 );
-
-
-
--- src/vendor.tsx
+++ src/vendor.tsx
@@ -3,9 +3,11 @@
 var Redux = require('redux');
 var React = require('react');
 var ReactDom = require('react-dom');
+var ReactRouter = require('react-router')
 
 export {
     Redux,
     React,
-    ReactDom
+    ReactDom,
+    ReactRouter
 };
