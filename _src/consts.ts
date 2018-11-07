8fbf65a561ccb11a234a8a580759ddb20f963ace
Дашкевич Ярослав Геннадьевич <Dashkevich.Y.Gen@sberbank.ru>
2018-11-07 11:15:50 +0700

    Clean old code

--- _src/consts.ts
+++
@@ -1,5 +0,0 @@
-export enum Filters {
-    SHOW_ALL = 0,
-    SHOW_ACTIVE = 1,
-    SHOW_COMPLETED = 2
-}
--- _typings/others.d.ts
+++
@@ -1,9 +0,0 @@
-declare var require: any;
-declare var exports: any;
-declare var vendor: any;
-
-declare var Redux: any;
-declare var React: any;
-declare var ReactDom: any;
-
-declare var Promise: any;
